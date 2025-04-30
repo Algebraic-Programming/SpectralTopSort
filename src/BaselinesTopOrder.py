5ece47fd046ad28ef73398641279348a09a83caf
“Raphael <raphael.steiner@huawei.com>
2025-04-30 15:13:40 +0200

    added Andras to Author list

--- src/BaselinesTopOrder.py
+++ src/BaselinesTopOrder.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-@author Raphael S. Steiner
+@author Pal Andras Papp, Raphael S. Steiner
 '''
 
 import collections
--- src/BenchmarkTopOrder.py
+++ src/BenchmarkTopOrder.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-@author Dimosthenis Pasadakis, Raphael S. Steiner
+@author Pal Andras Papp, Dimosthenis Pasadakis, Raphael S. Steiner
 '''
 
 import functools
